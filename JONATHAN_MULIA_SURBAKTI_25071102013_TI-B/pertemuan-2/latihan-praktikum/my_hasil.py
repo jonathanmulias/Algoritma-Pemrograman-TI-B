25280e784925fc12df2d0fdcaea6227179f74aed
jonathanmulias <surbaktijonathanmulia29@gmail.com>
2026-02-05 20:33:36 +0700

    LATIHAN PRAKTIKUM ALGORITMA PEMROGRAMAN PERTEMUAN 2

---
+++ JONATHAN_MULIA_SURBAKTI_25071102013_TI-B/pertemuan-2/latihan-praktikum/my_hasil.py
@@ -0,0 +1,15 @@
+import myMath as mm
+
+penjumlahan = mm.penjumlahan(8, 3)
+pengurangan = mm.pengurangan(5, 2)
+perkalian = mm.perkalian(5, 3)
+pembagian1 = mm.pembagian1(10, 2)
+pembagian2 = mm.pembagian2(10, 2)
+fibonaci = mm.fibonaci(5)
+
+print(penjumlahan)
+print(pengurangan)
+print(perkalian)
+print(pembagian1)
+print(pembagian2)
+print(fibonaci)
